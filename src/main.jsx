40d59871ff8ebdc8179ed845e691c304a86124d6
ibrahim <ibrahimjp630@gmail.com>
2025-08-30 14:34:35 +0530

    siu

--- src/main.jsx
+++ src/main.jsx
@@ -3,19 +3,19 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
-import Doctor from './components/Doctor/Doctor.jsx'
 import Pharmacy from './components/Pharmacy/Pharmacy.jsx'
 import Payment from './components/Payment/Payment.jsx'
 import Price from './components/Price/Price.jsx'
 import Footer from './components/Footer/Footer.jsx'
 import Navbar from './components/Navbar.jsx'
+import FindDoctorPage from './components/FindDoctorPage/FindDoctorPage.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Navbar/>
     <Router>
       <Routes>
         <Route path="/" element={<App />} />
-        <Route path="/Doctor" element={<Doctor />} />
+        <Route path="/Doctor" element={<FindDoctorPage />} />
         <Route path="/Pharmacy" element={<Pharmacy />} />
         <Route path="/Payment" element={<Payment />} />
         <Route path="/Price" element={<Price />} />
